357e5f7fe43cbf3a635c5e8aa7eb936b07aa4df2
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-03-26 14:27:22 +0800

    update

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,19 +1,39 @@
 import React, { useEffect } from 'react';
-import { Flex, Spinner, Text } from '@chakra-ui/react';
+import { Flex, Center, Text } from '@chakra-ui/react';
 import { useCart } from '../contexts/CartContext';
 
 const CartTotal = () => {
   const { cartTotal } = useCart();
 
   return (
-
-    <Flex w="100%" justifyContent="space-between" px={2} mb={2} flexWrap="wrap" alignItems="center">
-      <Text w="100%" fontWeight="bold"  fontSize={{ md: 'sm', lg: 'xl' }} textAlign="center" >
-        Total:
-      </Text>
-      <Text  w="100%" fontWeight="bold" display="flex" alignItems="center" fontSize={{ md: 'sm', lg: 'xl' }} textAlign="center">
-        HKD {cartTotal}
-      </Text>
+    <Flex
+      w="100%"
+      justifyContent="space-between"
+      px={2}
+      mb={2}
+      flexWrap="wrap"
+      alignItems="center"
+      textAlign="center"
+    >
+      <Center w="100%">
+        <Text
+          fontWeight="bold"
+          fontSize={{ md: 'sm', lg: 'md' }}
+          textAlign="center"
+        >
+          Total:
+        </Text>
+      </Center>
+      <Center w="100%">
+        <Text
+          fontWeight="bold"
+          alignItems="center"
+          fontSize={{ md: 'sm', lg: 'md' }}
+          textAlign="center"
+        >
+          HKD {cartTotal}
+        </Text>
+      </Center>
     </Flex>
   );
 };
